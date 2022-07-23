bd9972c180b8ffcad18bf299a855b15646212143
omidsotooni <mr.sotooni@yahoo.com>
2022-07-23 20:29:49 +0430

    create CategoriesController and adding index view

---
+++ EndPoint.Site/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopWithASP.NETCore.Application.Interfaces.FacadPatterns;
+
+namespace EndPoint.Site.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+
+        private readonly IProductFacad _productFacad;
+
+        public CategoriesController(IProductFacad productFacad)
+        {
+            _productFacad = productFacad;
+        }
+        public IActionResult Index(long? parentId)
+        {
+            return View(_productFacad.GetCategoriesService.Execute(parentId).Data);
+        }
+
+    }
+}
