78fda66e2a42b3f16fc2d352f4d15ad2b4c99222
Omid Sotooni <mr.sotooni@yahoo.com>
2023-03-20 14:26:58 +0330

    Add Utility Calss and Move pagination to Utility Calss

---
+++ Shop.Common/Utility.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop.Common
+{
+    public static class Utility
+    {
+        #region Methods
+        /// <summary>
+        /// Pagination
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="page"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="rowsCount"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> ToPaged<TSource>(this IEnumerable<TSource> source, int page, int pageSize, out int rowsCount)
+        {
+            rowsCount = source.Count();
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        #endregion
+    }
+}
